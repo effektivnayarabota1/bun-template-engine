90c984affc13fae1afecaa44f5bfc9f333a7c97f
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-08-05 19:25:38 +0300

    feat(test-app): render a bte page

--- src/test-app/src/app.js
+++ src/test-app/src/app.js
@@ -1,14 +1,28 @@
-import { create_ssr_component } from "svelte/internal";
+// import { create_ssr_component } from "@svelte/internal";
 
 import { FlatBte } from "templates";
+import { indexHtmlTemplate } from "templates";
+import { BtePageView } from "rewriter";
+
+const btePageView = new BtePageView(indexHtmlTemplate);
 
 export const app = async () => {
   const server = Bun.serve({
-    fetch(req) {
+    async fetch(req) {
+      // console.log("raw bte file from plugin:");
+      // console.log(FlatBte);
 
-      const bteRenderResult = FlatBte.render();
+      const rewritedHtmlPage = await btePageView.getPageHtml(FlatBte, {
+        color: "red",
+      });
 
+      console.log(rewritedHtmlPage);
 
+      return new Response(rewritedHtmlPage, {
+        headers: {
+          "Content-Type": "text/html",
+        },
+      });
     },
   });
   console.log(`bun server started at ${server.url.href}`);
