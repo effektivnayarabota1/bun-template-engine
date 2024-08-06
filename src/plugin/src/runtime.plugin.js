047290b3d9de2c05b234a46b15f478916ac3c70e
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-08-06 08:43:43 +0300

    refactor(plugin): change bte file extention to svelte

---
+++ src/plugin/src/runtime.plugin.js
@@ -0,0 +1,28 @@
+import { plugin } from "bun";
+
+export const btePlugin = () => {
+  return {
+    name: "bteRuntimePlugin",
+    async setup(build) {
+      const { compile } = await import("svelte/compiler");
+
+      build.onLoad({ filter: /\.svelte$/ }, async ({ path }) => {
+        const file = await Bun.file(path).text();
+
+        const result = compile(file, {
+          filename: path,
+          generate: "ssr",
+        });
+
+        const jsString = result.js.code;
+
+        return {
+          contents: jsString,
+          loader: "js",
+        };
+      });
+    },
+  };
+};
+
+await plugin(btePlugin());
